93fa6f6dfde742203c08110105af33a11b25bcb5
connerccook <42854861+connerccook@users.noreply.github.com>
2019-11-11 19:28:26 -0800

    Update Bank.cpp

--- Bank.cpp
+++ Bank.cpp
@@ -7,5 +7,5 @@
 ************************************************/
 
 Bank::Bank(const std::string & name,
-    const double & starting,
-    const double & salary) {} //defauilt constructor
+           const double & starting,
+           const double & salary) {} //defauilt constructor
